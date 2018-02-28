00a0b998479132e000a65023d9434e83467665ce
123p10 <obrake3@gmail.com>
2018-02-28 07:57:21 -0500

    Comp Sci Quiz

--- Python and Web/XAMPP/ICS-3U/PHP Loops and Bowtie/Home.php
+++ Python and Web/XAMPP/ICS-3U/PHP Loops and Bowtie/Home.php
@@ -24,7 +24,7 @@
 <?php
 	$a = 5;
 	for($r = 1;$r <= $a;$r++){
-		for($c = $a - $r;$c >= 0;$c--){
+		for($c = 0;$c <= $a-$r;$c++){
 			echo "*";
 		}
 		echo "<br>";
@@ -75,5 +75,69 @@
 			echo "*";
 		}
 		echo "<br>";
+	}
+?>
+
+<h2>Inverted Vertically Pyramid</h2><br>
+<?php
+	$a = 15;
+	for($i = $a;$i > 0;$i-=2){
+		for($c = 0;$c < ($a-$i)/2;$c++){
+			echo "&nbsp;&nbsp;";
+		}
+		for($b = 0;$b < $i;$b++){
+			echo "*";
+		}
+		echo "<br>";
+	}
+
+?>
+<h2>Inverted Horizontally Pyramid</h2><br>
+<?php
+	$a = 10;
+	for($i = 1;$i <= $a;$i++){
+		for($b = 0;$b < ($a-$i);$b++){
+			echo "&nbsp;&nbsp;";
+		}
+		for($c = 0;$c < $i;$c++){
+			echo "*";
+		}
+		echo "<br>";
+		
+	}
+
+?>
+<h2>Sentence Scaling</h2><br>
+<?php
+	$icon = "ABC";
+	$s = 3;
+	for($se = 0;$se < $s;$se++){
+		for($b = 0;$b < strlen($icon);$b++){
+			for($i = 0;$i < $s;$i++){
+				echo substr($icon,$b,1);
+			}
+		}
+		echo "<br>";
+	}
+?>
+<h2>Simple Icon Scaling</h2><br>
+<?php
+	$sc = 2;
+	$a = "";
+	$x = "";
+	$sp = "";
+	for($i = 0;$i < $sc;$i++){
+		$a .= "* ";
+		$x .= "x ";
+		$sp .= "&nbsp;&nbsp;&nbsp;";
+	}
+	for($i = 0;$i < $sc;$i++){
+		echo $a . $x . $a . "<br>";
+	}
+	for($i = 0;$i < $sc;$i++){
+		echo $sp . $x . $x . "<br>";
+	}
+	for($i = 0;$i < $sc;$i++){
+		echo $a . $sp . $a . "<br>";
 	}
 ?>
