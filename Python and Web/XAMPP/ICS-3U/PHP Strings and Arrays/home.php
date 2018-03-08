fa767a8b5fc01a91ad9e166188e05c667da0e692
123p10 <obrake3@gmail.com>
2018-03-08 12:12:58 -0500

    arrays and strings

--- Python and Web/XAMPP/ICS-3U/PHP Strings and Arrays/home.php
+++ Python and Web/XAMPP/ICS-3U/PHP Strings and Arrays/home.php
@@ -21,6 +21,26 @@ echo "Number of Vowels: " . $vt . "<br>Number of Consonants: " . $ct;
 ?>
 <br>
 <?php
-$in = "212223213";
-$arr = [$one,$two,$three];
+	$o = ["ABC","ABCA","ABCAB"];
+	for($l = 0;$l < count($o);$l++){
+		for($i = 0;$i < strlen($o[$l]);$i++){
+			$str = $o[$l];
+			if(strpos($str,$str[$i],$i+1) === false){
+				echo substr($o[$l],$i,1) . " is unique<br>";
+				break;
+			}
+		}
+	}
+?>
+<br>
+<?php
+	$orig = ["S","T","O","P"];
+	$test = $orig;
+	$test[0] = $orig[2];
+	$test[1] = $orig[3];
+	$test[3] = $orig[1];
+	$test[2] = $orig[0];
+	
+	echo $test[0] . $test[1] . $test[2] . $test[3];
+	
 ?>
